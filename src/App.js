cd4fd5efe0fc4ef04941e1d7cd910829eafd82e9
Matan <m.halevy@alumni.ubc.ca>
2016-12-27 01:39:39 -0800

    react components added

--- src/App.js
+++ src/App.js
@@ -1,3 +1,17 @@
+var BugRow = React.createClass({
+  render: function() {
+    return (
+      <tr>
+        <td>{this.props.id}</td>
+        <td>{this.props.bug.status}</td>
+        <td>{this.props.bug.priority}</td>
+        <td>{this.props.bug.owner}</td>
+        <td>{this.props.bug.title}</td>
+      </tr>
+    )
+  }
+})
+
 var BugFilter = React.createClass({
   render: function() {
     return (
@@ -8,8 +22,24 @@ var BugFilter = React.createClass({
 
 var BugTable =  React.createClass({
   render: function() {
+    var bugRows = this.props.bugs.map(function(bug) {
+      return <BugRow key={bug.id} bug={bug} />
+    })
     return (
-      <div>A table to list all the bugs</div>
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Status</th>
+            <th>Priority</th>
+            <th>Owner</th>
+            <th>Title</th>
+          </tr>
+        </thead>
+        <tbody>
+          {bugRows}
+        </tbody>
+      </table>
     )
   }
 })
@@ -26,14 +56,22 @@ var BugList = React.createClass({
   render: function() {
     return (
       <div>
+        <h1> Bug Tracker </h1>
         <BugFilter />
-        <BugTable />
+        <hr />
+        <BugTable bugs={bugData}/>
+        <hr />
         <BugAdd />
       </div>
     )
   }
 });
 
+
+var bugData = [
+  {id: 1, priority: 'P1', status: 'open', owner: 'Matan', title: 'App crashes on open'},
+  {id: 2, priority: 'P2', status: 'new', owner: 'Fred', title: 'Misaligned border on panel'},
+]
 
 ReactDOM.render(
   <BugList />,
--- static/App.js
+++ static/App.js
@@ -1,5 +1,41 @@
 'use strict';
 
+var BugRow = React.createClass({
+  displayName: 'BugRow',
+
+  render: function render() {
+    return React.createElement(
+      'tr',
+      null,
+      React.createElement(
+        'td',
+        null,
+        this.props.id
+      ),
+      React.createElement(
+        'td',
+        null,
+        this.props.bug.status
+      ),
+      React.createElement(
+        'td',
+        null,
+        this.props.bug.priority
+      ),
+      React.createElement(
+        'td',
+        null,
+        this.props.bug.owner
+      ),
+      React.createElement(
+        'td',
+        null,
+        this.props.bug.title
+      )
+    );
+  }
+});
+
 var BugFilter = React.createClass({
   displayName: 'BugFilter',
 
@@ -16,10 +52,50 @@ var BugTable = React.createClass({
   displayName: 'BugTable',
 
   render: function render() {
+    var bugRows = this.props.bugs.map(function (bug) {
+      return React.createElement(BugRow, { key: bug.id, bug: bug });
+    });
     return React.createElement(
-      'div',
+      'table',
       null,
-      'A table to list all the bugs'
+      React.createElement(
+        'thead',
+        null,
+        React.createElement(
+          'tr',
+          null,
+          React.createElement(
+            'th',
+            null,
+            'ID'
+          ),
+          React.createElement(
+            'th',
+            null,
+            'Status'
+          ),
+          React.createElement(
+            'th',
+            null,
+            'Priority'
+          ),
+          React.createElement(
+            'th',
+            null,
+            'Owner'
+          ),
+          React.createElement(
+            'th',
+            null,
+            'Title'
+          )
+        )
+      ),
+      React.createElement(
+        'tbody',
+        null,
+        bugRows
+      )
     );
   }
 });
@@ -43,11 +119,20 @@ var BugList = React.createClass({
     return React.createElement(
       'div',
       null,
+      React.createElement(
+        'h1',
+        null,
+        ' Bug Tracker '
+      ),
       React.createElement(BugFilter, null),
-      React.createElement(BugTable, null),
+      React.createElement('hr', null),
+      React.createElement(BugTable, { bugs: bugData }),
+      React.createElement('hr', null),
       React.createElement(BugAdd, null)
     );
   }
 });
+
+var bugData = [{ id: 1, priority: 'P1', status: 'open', owner: 'Matan', title: 'App crashes on open' }, { id: 2, priority: 'P2', status: 'new', owner: 'Fred', title: 'Misaligned border on panel' }];
 
 ReactDOM.render(React.createElement(BugList, null), document.getElementById('main'));
